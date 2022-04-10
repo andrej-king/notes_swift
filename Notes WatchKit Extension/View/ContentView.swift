96334a2ef8dfd962fd2732e91912d3e6403c880f
Andrej Pedchenko <graduadu@gmail.com>
2022-04-10 17:01:49 +0300

    Added Note model.

--- Notes WatchKit Extension/View/ContentView.swift
+++ Notes WatchKit Extension/View/ContentView.swift
@@ -1,11 +1,22 @@
 import SwiftUI
 
 struct ContentView: View {
+    // MARK: - PROPERTY
+    
+    @State private var notes: [Note] = [Note]()
+    @State private var text: String = ""
+    
+    // MARK: - FUNCTION
+    
+    // MARK: - BODY
+    
     var body: some View {
         Text("Hello, World!")
             .padding()
     }
 }
+
+// MARK: - PREVIEW
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
