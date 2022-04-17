efcbf1b4f5cce553d6d0aca952bd84700c2feca4
Andrej Pedchenko <graduadu@gmail.com>
2022-04-17 22:13:07 +0300

    Added custom binding for read and write value by click slider

--- Notes WatchKit Extension/View/SettingsView.swift
+++ Notes WatchKit Extension/View/SettingsView.swift
@@ -6,6 +6,13 @@ struct SettingsView: View {
     @AppStorage("lineCount") var lineCount: Int = 1
     @State private var value: Float = 1.0
     
+    // MARK: - FUNCTION
+    
+    // update line count in main view
+    func update() {
+        lineCount = Int(value)
+    }
+    
     // MARK: - BODY
     
     var body: some View {
@@ -18,7 +25,12 @@ struct SettingsView: View {
                 .fontWeight(.bold)
             
             // SLIDER
-            Slider(value: $value, in: 1...4, step: 1)
+            Slider(value: Binding(get: { // custom binding by change slider value
+                self.value
+            }, set: {(newValue) in
+                self.value = newValue
+                self.update()
+            }), in: 1...4, step: 1)
                 .accentColor(.accentColor)
         } //: VSTACK
     }
