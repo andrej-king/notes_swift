a63cafabb497f028b5140f86cce3ac52f5128191
Andrej Pedchenko <graduadu@gmail.com>
2022-04-17 20:09:05 +0300

    Moved header section to separate view. Created view with credits (developer info).

---
+++ Notes WatchKit Extension/View/CreaditsView.swift
@@ -0,0 +1,44 @@
+import SwiftUI
+
+struct CreaditsView: View {
+    // MARK: - PROPERTY
+    @State private var randomNumber: Int = Int.random(in: 1..<4)
+    
+    private var randomImage: String {
+        return "developer-no\(randomNumber)"
+    }
+    
+    // MARK: - BODy
+    
+    var body: some View {
+        VStack(spacing: 3) {
+            // PROFILE IMAGE
+            Image(randomImage)
+                .resizable()
+                .scaledToFit()
+                .layoutPriority(1)
+            
+            // HEADER
+            HeaderView(title: "Creadits")
+            
+            // CONTENT
+            Text("John Doe")
+                .foregroundColor(.primary)
+                .fontWeight(.bold)
+            
+            Text("Developer")
+                .font(.footnote)
+                .foregroundColor(.secondary)
+                .fontWeight(.light)
+            
+        } //: VSTACK
+    }
+}
+
+// MARK: - PREVIEW
+
+struct CreaditsView_Previews: PreviewProvider {
+    static var previews: some View {
+        CreaditsView()
+    }
+}
--- Notes WatchKit Extension/View/DetailView.swift
+++ Notes WatchKit Extension/View/DetailView.swift
@@ -12,16 +12,7 @@ struct DetailView: View {
     var body: some View {
         VStack(alignment: .center, spacing: 3) {
             // HEADER
-            HStack {
-                Capsule()
-                    .frame(height: 1)
-                
-                Image(systemName: "note.text")
-                
-                Capsule()
-                    .frame(height: 1)
-            } //: HSTACK
-            .foregroundColor(.accentColor)
+            HeaderView(title: "")
             
             // CONTENT
             Spacer()
